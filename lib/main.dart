b10d9cd763fba4d5019cde1246c735f654e329a2
andrew <rozumiakandrij1@gmail.com>
2024-12-20 16:57:43 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+/// Головний віджет додатка.
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
+  }
+}
+
+/// Головний екран програми.
+class MainScreen extends StatefulWidget {
+  @override
+  _ColorState createState() => _ColorState();
+}
+class _ColorState extends State<MainScreen> {
+  Color _backgroundColor = const Color(0xFFFFFFFF);
+  void _changedBackgroundColor() {
+    final randomColor = Random();
+    setState(() {
+      _backgroundColor = Color.fromARGB(
+        255,
+        randomColor.nextInt(256),
+        randomColor.nextInt(256),
+        randomColor.nextInt(256),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _changedBackgroundColor,
+      child: CupertinoPageScaffold(
+        backgroundColor: _backgroundColor,
+        child: const Center(
+          child: Text(
+            'Hello there',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
